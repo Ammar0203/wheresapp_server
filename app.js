24c7c745d47fe6fc383d60ca019fdee97724759c
kuddo <iluffy120@gmail.com>
2023-12-23 12:01:25 +0300

    first

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+require('./socket-handler')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +25,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/auth', require('./routes/auth'));
+
+app.use((err, req, res, next) => {
+  if(req.get('accept').includes('json')) {
+    return next(createError(404))
+  }
+  res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.use((err, req, res, next) => {
+  if(err.name === 'MongoError' || err.name === 'ValidationError' || err.name === 'castError') {
+    err.status = 422
+  }
+  res.status(err.status || 500).json({message: err.message || 'some error occured.'})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +56,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mongoose.connect(process.env.DB_URL)
+console.log('Connected successfully.')
 
 module.exports = app;
---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const createError = require('http-errors')
+
+exports.socket = (socket, next) => {
+  if(!socket.handshake.query || !socket.handshake.query.token){
+    return next(createError(401, 'auth_error'))
+  }
+
+  jwt.verify(socket.handshake.query.token, process.env.JWT_SECRET, (err, decoded) => {
+    if(err) return next(createError(401, 'auth_error'))
+    User.findById(decoded.id)
+    .then(user => {
+      if(!user) return next(createError(401, 'auth_error'))
+
+      socket.user = user
+      next()
+    })
+    .catch(next)
+  })
+}
